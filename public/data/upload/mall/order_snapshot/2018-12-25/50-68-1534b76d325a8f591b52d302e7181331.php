65054c2b98d34d161444c3d286ffc2dada0ad7fc
dragonflylxp <dragonflylxp@gmail.com>
2018-12-26 11:25:44 +0800

    更新数据库备份文件

---
+++ public/data/upload/mall/order_snapshot/2018-12-25/50-68-1534b76d325a8f591b52d302e7181331.php
@@ -0,0 +1,52 @@
+<?php defined('Inshopec') or exit('Access Invalid!');?><div class="default"><p>
+	<img src="http://manpay.sicpay.com/data/upload/mall/store/goods/250/250_05977740032683454_1280.jpg" alt="image" /><img src="http://manpay.sicpay.com/data/upload/mall/store/goods/250/250_05977740138391977_1280.jpg" alt="image" /><img src="http://manpay.sicpay.com/data/upload/mall/store/goods/250/250_05977740180678158_1280.jpg" alt="image" /><img src="http://manpay.sicpay.com/data/upload/mall/store/goods/250/250_05977740234562309_1280.jpg" alt="image" /><img src="http://manpay.sicpay.com/data/upload/mall/store/goods/250/250_05977740277118449_1280.jpg" alt="image" />
+</p>
+<p>
+	</p><div class="title" style="border:0px;font-weight:bold;font-size:18px;font-family:proxima_nova, Arial, &quot;vertical-align:baseline;margin:0px 0px 20px;padding:0px;background-color:#FFFFFF;">
+		<span style="font-style:inherit;font-weight:inherit;font-size:inherit;line-height:inherit;font-family:inherit;vertical-align:baseline;">詳細介紹</span>
+	</div>
+	<div class="tabBody" style="border:0px;font-size:15px;font-family:proxima_nova, Arial, &quot;vertical-align:baseline;margin:0px;padding:0px;background-color:#FFFFFF;">
+		<div style="border:0px;font-style:inherit;font-weight:inherit;font-size:inherit;font-family:inherit;vertical-align:baseline;margin:0px;padding:0px;">
+			<p style="font-style:inherit;font-weight:inherit;font-size:inherit;font-family:inherit;vertical-align:baseline;">
+				產品規格及特色<br />
+型號：AeBall<br />
+類別：頂級版<br />
+尺寸：125(直徑) x 119(高) mm<br />
+重量：355g<br />
+供電介面：Mini USB<br />
+供電規格：220V - 230V, 50Hz - 60Hz<br />
+耗電量：2.8W<br />
+安全認證：CE / FCC / PSE / KC<br />
+使用嶄新專利技術 AICI (Airvita Ions-ozone) 產生負離子<br />
+半永久性，不需換濾芯，無濾網設計，可免除經常清洗及更換的功夫，只需每月清洗濾芯即可<br />
+低臭氧排放<br />
+輕巧無線設計，USB 介面充電，而且耗電量低<br />
+有效淨化室內空氣，消除甲醛等有害氣體對人健康的傷害<br />
+功率大：在一秒鐘內放出 1860 億個電子，可淨化被污染的空氣，而且每 1cc 空氣放出的負氧離子就有 2 百萬個<br />
+利用 LED 電路晶片，製造出類似於大自然的雷電效應，利用靜電高壓的原理而釋放出負離子<br />
+<br />
+核心系統：<br />
+空氣淨化系統：負離子式<br />
+技術：AICI Complex Plasma Anion<br />
+負離子生產數量：每立方空氣 2 百萬粒<br />
+工作模式：正常模式 / 加强模式<br />
+最大可用範圍：700 呎<br />
+適用地方：室內 (家中 / 辦公室)<br />
+電池規格：<br />
+<br />
+內置電池類別：Li-ion<br />
+電池容量：3000mA<br />
+充電時間：~ 3 小時 (由零到充滿)<br />
+使用時間：~ 7 小時<br />
+產品功能：<br />
+<br />
+切換工作模式：自動 / 手動<br />
+空氣質素檢測：有<br />
+夜燈：有<br />
+觸控面版：有<br />
+殺菌：能有效殺死有害細菌、黴菌、感冒病毒、(H5N1)等傳染病毒並抑制病菌的侵襲，分解苯、一氧化碳、瓦斯等有毒氣體。(開啟 10 分鐘後：大腸桿菌可減少 73.2%，葡萄球菌可減少 92.8%)<br />
+除臭：消除煙臭、裝潢及化學藥品臭味、食物的殘餘味、腐壞味、霉味、阿摩尼亞（大小便）等臭味。(開啟 2 小時後：可由 51.1 PPM 減少為 10 PPM)<br />
+淨化空氣：快速、有效地電解空氣中的煙塵等，去除室內異味、煙臭味、室內污染空氣迅速轉化成新鮮清淨的空氣
+			</p>
+		</div>
+	</div></div>
